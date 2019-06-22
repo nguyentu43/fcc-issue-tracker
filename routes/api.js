886d436007b9f73756fe23242b973b2598651249
Glitch (picturesque-petroleum) <none>
2019-06-22 16:35:36 +0000

    🌧👡 Checkpoint ./routes/api.js:5024683/936 ./package.json:5024683/12251

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,38 @@
 'use strict';
 
 var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
+const mongoose = require('mongoose');
+const CONNECTION_STRING = process.env.DB;
+const moment = require('moment');
+mongoose.connect(CONNECTION_STRING);
 
-const CONNECTION_STRING = process.env.DB; 
-const client = new MongoClient(CONNECTION_STRING);
+const schema = mongoose.Schema({
+  issue_title: {
+    type: String,
+    required: true
+  }, 
+  issue_text: {
+    type: String,
+    required: true
+  }, 
+  created_by: {
+    type: Date,
+    required: true
+  },
+  assigned_to: String,
+  status_text: String,
+  open: {
+    type: Boolean,
+    default: false
+  },
+  update_on: Date
+});
+
+schema.pre('save', function(){
+  this.update_on = moment().format("YYYY-MM-DD HH:mm:ss");
+});
+
+const Issue = mongoose.model('Issue', schema);
 
 module.exports = function (app) {
 
@@ -26,7 +53,7 @@ module.exports = function (app) {
     
     .post(function (req, res){
       var project = req.params.project;
-      
+      const issue = new Issue({})
     })
     
     .put(function (req, res){
